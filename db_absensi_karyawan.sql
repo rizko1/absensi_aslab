25c30681e79a798ef5f1530253bafcba4ff5e6c6
rizko1 <rizkoalamin56@gmail.com>
2025-07-24 15:04:01 +0700

    p

---
+++ db_absensi_karyawan.sql
@@ -0,0 +1,581 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 08 Nov 2024 pada 01.21
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `absensi_aslab`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `attendances`
+--
+
+CREATE TABLE `attendances` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `user_id` char(26) NOT NULL,
+  `barcode_id` bigint(20) UNSIGNED DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  `time_in` time DEFAULT NULL,
+  `time_out` time DEFAULT NULL,
+  `shift_id` bigint(20) UNSIGNED DEFAULT NULL,
+  `latitude` double DEFAULT NULL,
+  `longitude` double DEFAULT NULL,
+  `status` enum('present','late','excused','sick','absent') NOT NULL DEFAULT 'absent',
+  `note` varchar(255) DEFAULT NULL,
+  `attachment` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `barcodes`
+--
+
+CREATE TABLE `barcodes` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `value` varchar(255) NOT NULL,
+  `latitude` double NOT NULL DEFAULT 0,
+  `longitude` double NOT NULL DEFAULT 0,
+  `radius` double NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `barcodes`
+--
+
+INSERT INTO `barcodes` (`id`, `name`, `value`, `latitude`, `longitude`, `radius`, `created_at`, `updated_at`) VALUES
+(1, 'Barcode 1', '8885133434678', 60.475141, -61.581115, 50, '2024-11-08 00:11:55', '2024-11-08 00:11:55');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `cache`
+--
+
+CREATE TABLE `cache` (
+  `key` varchar(255) NOT NULL,
+  `value` mediumtext NOT NULL,
+  `expiration` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `cache`
+--
+
+INSERT INTO `cache` (`key`, `value`, `expiration`) VALUES
+('580071baad7b227a20e85017933acecb', 'i:1;', 1731025261),
+('580071baad7b227a20e85017933acecb:timer', 'i:1731025261;', 1731025261);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `cache_locks`
+--
+
+CREATE TABLE `cache_locks` (
+  `key` varchar(255) NOT NULL,
+  `owner` varchar(255) NOT NULL,
+  `expiration` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `divisions`
+--
+
+CREATE TABLE `divisions` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `divisions`
+--
+
+INSERT INTO `divisions` (`id`, `name`, `created_at`, `updated_at`) VALUES
+(1, 'Divisi 1', '2024-11-08 00:11:55', '2024-11-08 00:11:55'),
+(2, 'Divisi 2', '2024-11-08 00:11:55', '2024-11-08 00:11:55'),
+(3, 'Divisi 3', '2024-11-08 00:11:55', '2024-11-08 00:11:55'),
+(4, 'Divisi 4', '2024-11-08 00:11:55', '2024-11-08 00:11:55');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `educations`
+--
+
+CREATE TABLE `educations` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `educations`
+--
+
+INSERT INTO `educations` (`id`, `name`, `created_at`, `updated_at`) VALUES
+(1, 'SD', '2024-11-08 00:11:55', '2024-11-08 00:11:55'),
+(2, 'SMP', '2024-11-08 00:11:55', '2024-11-08 00:11:55'),
+(3, 'SMA', '2024-11-08 00:11:55', '2024-11-08 00:11:55'),
+(4, 'SMK', '2024-11-08 00:11:55', '2024-11-08 00:11:55'),
+(5, 'D1', '2024-11-08 00:11:55', '2024-11-08 00:11:55'),
+(6, 'D2', '2024-11-08 00:11:55', '2024-11-08 00:11:55'),
+(7, 'D3', '2024-11-08 00:11:55', '2024-11-08 00:11:55'),
+(8, 'D4', '2024-11-08 00:11:55', '2024-11-08 00:11:55'),
+(9, 'S1', '2024-11-08 00:11:55', '2024-11-08 00:11:55'),
+(10, 'S2', '2024-11-08 00:11:55', '2024-11-08 00:11:55'),
+(11, 'S3', '2024-11-08 00:11:55', '2024-11-08 00:11:55');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `failed_jobs`
+--
+
+CREATE TABLE `failed_jobs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `uuid` varchar(255) NOT NULL,
+  `connection` text NOT NULL,
+  `queue` text NOT NULL,
+  `payload` longtext NOT NULL,
+  `exception` longtext NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `jobs`
+--
+
+CREATE TABLE `jobs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `queue` varchar(255) NOT NULL,
+  `payload` longtext NOT NULL,
+  `attempts` tinyint(3) UNSIGNED NOT NULL,
+  `reserved_at` int(10) UNSIGNED DEFAULT NULL,
+  `available_at` int(10) UNSIGNED NOT NULL,
+  `created_at` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `job_batches`
+--
+
+CREATE TABLE `job_batches` (
+  `id` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `total_jobs` int(11) NOT NULL,
+  `pending_jobs` int(11) NOT NULL,
+  `failed_jobs` int(11) NOT NULL,
+  `failed_job_ids` longtext NOT NULL,
+  `options` mediumtext DEFAULT NULL,
+  `cancelled_at` int(11) DEFAULT NULL,
+  `created_at` int(11) NOT NULL,
+  `finished_at` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `job_titles`
+--
+
+CREATE TABLE `job_titles` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `job_titles`
+--
+
+INSERT INTO `job_titles` (`id`, `name`, `created_at`, `updated_at`) VALUES
+(1, 'Manager', '2024-11-08 00:11:55', '2024-11-08 00:11:55'),
+(2, 'Staff', '2024-11-08 00:11:55', '2024-11-08 00:11:55'),
+(3, 'Accounting', '2024-11-08 00:11:55', '2024-11-08 00:11:55'),
+(4, 'HRD', '2024-11-08 00:11:55', '2024-11-08 00:11:55'),
+(5, 'IT', '2024-11-08 00:11:55', '2024-11-08 00:11:55');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '0001_01_01_000000_create_divisions_table', 1),
+(2, '0001_01_01_000000_create_educations_table', 1),
+(3, '0001_01_01_000000_create_job_titles_table', 1),
+(4, '0001_01_01_000000_create_users_table', 1),
+(5, '0001_01_01_000001_add_two_factor_columns_to_users_table', 1),
+(6, '0001_01_01_000001_create_cache_table', 1),
+(7, '0001_01_01_000002_create_jobs_table', 1),
+(8, '2024_06_08_023152_create_personal_access_tokens_table', 1),
+(9, '2024_06_09_113236_create_barcodes_table', 1),
+(10, '2024_06_16_092112_create_shifts_table', 1),
+(11, '2024_06_17_113814_create_attendances_table', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `password_reset_tokens`
+--
+
+CREATE TABLE `password_reset_tokens` (
+  `email` varchar(255) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `personal_access_tokens`
+--
+
+CREATE TABLE `personal_access_tokens` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `tokenable_type` varchar(255) NOT NULL,
+  `tokenable_id` char(36) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `token` varchar(64) NOT NULL,
+  `abilities` text DEFAULT NULL,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `expires_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `sessions`
+--
+
+CREATE TABLE `sessions` (
+  `id` varchar(255) NOT NULL,
+  `user_id` char(26) DEFAULT NULL,
+  `ip_address` varchar(45) DEFAULT NULL,
+  `user_agent` text DEFAULT NULL,
+  `payload` longtext NOT NULL,
+  `last_activity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `sessions`
+--
+
+INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
+('lFD2Jq5IK2sVyb7CPZn5O5khbgfJPdXTziKpjNdY', '01jc4hszxx1spywkarsjqnf6dw', '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36', 'YTo2OntzOjY6Il90b2tlbiI7czo0MDoieFNkcmw5RlE4Qk1LVllpM2wxQjZIdjZNRjQ0UWQ4VmZ0dmU1dVFBTCI7czozOiJ1cmwiO2E6MTp7czo4OiJpbnRlbmRlZCI7czoyMToiaHR0cDovLzEyNy4wLjAuMTo4MDAwIjt9czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6NTM6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9hZG1pbi9pbXBvcnQtZXhwb3J0L2F0dGVuZGFuY2VzIjt9czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo1MDoibG9naW5fd2ViXzU5YmEzNmFkZGMyYjJmOTQwMTU4MGYwMTRjN2Y1OGVhNGUzMDk4OWQiO3M6MjY6IjAxamM0aHN6eHgxc3B5d2thcnNqcW5mNmR3IjtzOjIxOiJwYXNzd29yZF9oYXNoX3NhbmN0dW0iO3M6NjA6IiQyeSQxMiREMmtRMnZrS1IzcUdhTDVhcG0yenN1OWFPUjRMYWplNFhEaWRuSEVaTmZZNDJKLi82S213MiI7fQ==', 1731025230);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `shifts`
+--
+
+CREATE TABLE `shifts` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `start_time` time NOT NULL,
+  `end_time` time DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `shifts`
+--
+
+INSERT INTO `shifts` (`id`, `name`, `start_time`, `end_time`, `created_at`, `updated_at`) VALUES
+(1, 'Shift 2', '19:00:00', '02:00:00', '2024-11-08 00:11:55', '2024-11-08 00:11:55'),
+(2, 'Shift 1', '08:00:00', '17:00:00', '2024-11-08 00:11:55', '2024-11-08 00:11:55');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
+--
+
+CREATE TABLE `users` (
+  `id` char(26) NOT NULL,
+  `nim` varchar(255) DEFAULT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `gender` enum('male','female') NOT NULL,
+  `birth_date` date DEFAULT NULL,
+  `birth_place` varchar(255) DEFAULT NULL,
+  `address` varchar(255) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `education_id` bigint(20) UNSIGNED DEFAULT NULL,
+  `division_id` bigint(20) UNSIGNED DEFAULT NULL,
+  `job_title_id` bigint(20) UNSIGNED DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `two_factor_secret` text DEFAULT NULL,
+  `two_factor_recovery_codes` text DEFAULT NULL,
+  `two_factor_confirmed_at` timestamp NULL DEFAULT NULL,
+  `raw_password` varchar(255) DEFAULT NULL,
+  `group` enum('user','admin','superadmin') NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `profile_photo_path` varchar(2048) DEFAULT NULL,
+  `remember_token` varchar(100) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `users`
+--
+
+INSERT INTO `users` (`id`, `nim`, `name`, `email`, `phone`, `gender`, `birth_date`, `birth_place`, `address`, `city`, `education_id`, `division_id`, `job_title_id`, `password`, `two_factor_secret`, `two_factor_recovery_codes`, `two_factor_confirmed_at`, `raw_password`, `group`, `email_verified_at`, `profile_photo_path`, `remember_token`, `created_at`, `updated_at`) VALUES
+('01jc4hszxx1spywkarsjqnf6dw', '0000000000000000', 'Super Admin', 'superadmin@example.com', '00000000000', 'male', NULL, NULL, '', '', NULL, NULL, NULL, '$2y$12$D2kQ2vkKR3qGaL5apm2zsu9aOR4Laje4XDidnHEZNfY42J./6Kmw2', NULL, NULL, NULL, 'superadmin', 'superadmin', '2024-11-08 00:11:55', NULL, 'kNknHiwk6X', '2024-11-08 00:11:55', '2024-11-08 00:11:55'),
+('01jc4ht03xpg7vq9pvj7k9ayc5', '0000000000000000', 'Admin', 'admin@example.com', '00000000000', 'male', NULL, NULL, '', '', NULL, NULL, NULL, '$2y$12$nqAqrUiMb/16SYigiL9gIuIsrfSOeKKtIDn.fETK9qzANnUN0iSn6', NULL, NULL, NULL, 'admin', 'admin', '2024-11-08 00:11:55', NULL, 'LhQXoMZSR3', '2024-11-08 00:11:55', '2024-11-08 00:11:55');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `attendances`
+--
+ALTER TABLE `attendances`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `attendances_user_id_foreign` (`user_id`),
+  ADD KEY `attendances_barcode_id_foreign` (`barcode_id`),
+  ADD KEY `attendances_shift_id_foreign` (`shift_id`);
+
+--
+-- Indeks untuk tabel `barcodes`
+--
+ALTER TABLE `barcodes`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `barcodes_value_unique` (`value`);
+
+--
+-- Indeks untuk tabel `cache`
+--
+ALTER TABLE `cache`
+  ADD PRIMARY KEY (`key`);
+
+--
+-- Indeks untuk tabel `cache_locks`
+--
+ALTER TABLE `cache_locks`
+  ADD PRIMARY KEY (`key`);
+
+--
+-- Indeks untuk tabel `divisions`
+--
+ALTER TABLE `divisions`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `divisions_name_unique` (`name`);
+
+--
+-- Indeks untuk tabel `educations`
+--
+ALTER TABLE `educations`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `educations_name_unique` (`name`);
+
+--
+-- Indeks untuk tabel `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
+
+--
+-- Indeks untuk tabel `jobs`
+--
+ALTER TABLE `jobs`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `jobs_queue_index` (`queue`);
+
+--
+-- Indeks untuk tabel `job_batches`
+--
+ALTER TABLE `job_batches`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `job_titles`
+--
+ALTER TABLE `job_titles`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `job_titles_name_unique` (`name`);
+
+--
+-- Indeks untuk tabel `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `password_reset_tokens`
+--
+ALTER TABLE `password_reset_tokens`
+  ADD PRIMARY KEY (`email`);
+
+--
+-- Indeks untuk tabel `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
+
+--
+-- Indeks untuk tabel `sessions`
+--
+ALTER TABLE `sessions`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `sessions_user_id_index` (`user_id`),
+  ADD KEY `sessions_last_activity_index` (`last_activity`);
+
+--
+-- Indeks untuk tabel `shifts`
+--
+ALTER TABLE `shifts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`),
+  ADD KEY `users_education_id_foreign` (`education_id`),
+  ADD KEY `users_division_id_foreign` (`division_id`),
+  ADD KEY `users_job_title_id_foreign` (`job_title_id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `attendances`
+--
+ALTER TABLE `attendances`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `barcodes`
+--
+ALTER TABLE `barcodes`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `divisions`
+--
+ALTER TABLE `divisions`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT untuk tabel `educations`
+--
+ALTER TABLE `educations`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT untuk tabel `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `jobs`
+--
+ALTER TABLE `jobs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `job_titles`
+--
+ALTER TABLE `job_titles`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT untuk tabel `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT untuk tabel `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `shifts`
+--
+ALTER TABLE `shifts`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `attendances`
+--
+ALTER TABLE `attendances`
+  ADD CONSTRAINT `attendances_barcode_id_foreign` FOREIGN KEY (`barcode_id`) REFERENCES `barcodes` (`id`),
+  ADD CONSTRAINT `attendances_shift_id_foreign` FOREIGN KEY (`shift_id`) REFERENCES `shifts` (`id`),
+  ADD CONSTRAINT `attendances_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Ketidakleluasaan untuk tabel `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_division_id_foreign` FOREIGN KEY (`division_id`) REFERENCES `divisions` (`id`),
+  ADD CONSTRAINT `users_education_id_foreign` FOREIGN KEY (`education_id`) REFERENCES `educations` (`id`),
+  ADD CONSTRAINT `users_job_title_id_foreign` FOREIGN KEY (`job_title_id`) REFERENCES `job_titles` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
